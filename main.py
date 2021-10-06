eb14f985764c5cb05ed7ddddd95027365dcf96aa
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-10-06 03:55:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from src.init import app
 #from src.admin import admin
-from src.clean_up import clean_up
 from src.endpoints.api import route_audio, route_download
 from src.endpoints.api import route_session_search_status, route_session_new
 from src.endpoints.api import route_file_search_results, route_file_search_status, route_file_search_start, route_file_upload
