85ef122cbc6d5e30507b18db28898c0920a935ea
hogshead-revival-widow <67462744+hogshead-revival-widow@users.noreply.github.com>
2021-10-07 23:31:52 +0200

    init aufgeteilt

--- src/endpoints/user.py
+++ src/endpoints/user.py
@@ -1,10 +1,11 @@
 from flask import render_template
-from ..init import app, frontend_consts
+from ..init import app
+from ..consts import EXPORT_FRONTEND
 from ..clean_up import clean_up
+
 
 @ app.route('/')
 @ app.route('/zerhacken')
 def route_index():
     clean_up()
-    return render_template('index.html', consts=frontend_consts)
-
+    return render_template('index.html', consts=EXPORT_FRONTEND)
